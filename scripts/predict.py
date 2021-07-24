2d58cf84b618ade1de70cf5381e054ed62a64433
lorenzo <lorenzovitale89@gmail.com>
2021-07-24 17:09:29 +0200

    data changes

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,7 +1,10 @@
 import argparse
 import os
 import re
+import sys
 import time
+import yaml
+from pathlib import  Path
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
 
@@ -33,8 +36,9 @@ def output_path(path):
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--input', '-i', required=True, type=input_path, nargs='+',
-                        help='paths to protein structures')
+    parser.add_argument('--destDataset', required=True, type=input_path,
+                        help='path to the processed database')
+
     parser.add_argument('--model', '-m', type=input_path, default=model_path,
                         help='path to the .hdf file with trained model.')
     parser.add_argument('--dirname_pattern', type=str,
@@ -43,7 +47,7 @@ def parse_args():
     parser.add_argument('--output', '-o', type=output_path,
                         help='name for the output directory. If not specified, '
                              '"pockets_<YYYY>-<MM>-<DD>" will be used')
-    parser.add_argument('--format', '-f', default='mol2',
+    parser.add_argument('--format', '-f', default='pdb',
                         help='input format; can be any format for 3D structures'
                              ' supported by Open Babel')
     parser.add_argument('--verbose', '-v', action='store_true',
@@ -55,11 +59,29 @@ def parse_args():
     parser.add_argument('--device', '-d', type=str, default='gpu', const='gpu', choices=['gpu', 'cpu'],
                         nargs='?', help='device')
 
+    parser.add_argument("-r", "--runconfig", dest='runconfig', type=str, required=True,
+                        help=f"The run config yaml file")
+
     return parser.parse_args()
+
+class LoadConfig:
+    def __init__(self, path):
+        runconfig = yaml.safe_load(open(path, 'r'))
+        self.test_ids = runconfig['test']
 
 
 def main():
     args = parse_args()
+
+    runconfig = args.runconfig
+
+    config = LoadConfig(runconfig)
+    test_ids = config.test_ids
+
+    destData = args.destDataset
+    if not os.path.exists(destData):
+        print(f"{destData} does not exist.", file=sys.stderr)
+        sys.exit(-1)
 
     if args.device == 'cpu':
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
@@ -77,30 +99,19 @@ def main():
     model = UNet.load_model(args.model, scale=args.scale, max_dist=args.max_dist,
                             featurizer=Featurizer(save_molecule_codes=False))
 
-    if args.verbose:
-        progress_bar = tqdm
-    else:
-        progress_bar = iter
+    for test_id in test_ids:
 
-    for path in progress_bar(args.input):
-        match = re.match(args.dirname_pattern, path)
-        if not match:
-            raise ValueError('Cannot extract name from %s. '
-                             'Please specify correct --namedir_pattern' % path)
-        dirname = os.path.join(args.output, match.groups()[0])
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-
+        path = str(Path(destData) / test_id / f"{test_id}_selected.pdb")
         mol = next(readfile(args.format, path))
 
         # predict pockets and save them as separate mol2 files
         pockets = model.predict_pocket_atoms(mol)
         for i, pocket in enumerate(pockets):
-            pocket.write('mol2', os.path.join(dirname, 'pocket%i.mol2' % i), overwrite=True)
+            pocket.write('pdb', os.path.join(args.output, f'{test_id}_predictions.pdb'), overwrite=True)
 
         # save pocket probability as density map (UCSF Chimera format)
         density, origin, step = model.pocket_density_from_mol(mol)
-        model.save_density_as_cmap(density, origin, step, fname=os.path.join(dirname, 'pockets.cmap'))
+        model.save_density_as_cmap(density, origin, step, fname=os.path.join(args.output, 'pockets.cmap'))
 
 
 if __name__ == '__main__':
